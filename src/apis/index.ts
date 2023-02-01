d32fae00eafe742953ac58908c7aff57e03b72c3
Monica Buciuman <moni_mon88@yahoo.com>
2023-02-01 17:30:14 +0200

    enable endpoint

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,16 +1,16 @@
-import { countriesMock } from "../mockData";
+// import { countriesMock } from "../mockData";
 import { CountryType } from "../model/CountryType";
 
 export const fetchCountries = async (
   setCountries: (countries: CountryType[]) => void
 ) => {
-  //   const data = await fetch(
-  //     "https://restcountries.com/v2/all?fields=name,region,area",
-  //     {
-  //       method: "GET",
-  //     }
-  //   );
-  //   const jsonData = await data.json();
-  //   setCountries(jsonData);
-  setCountries(countriesMock);
+  const data = await fetch(
+    "https://restcountries.com/v2/all?fields=name,region,area",
+    {
+      method: "GET",
+    }
+  );
+  const jsonData = await data.json();
+  setCountries(jsonData);
+  // setCountries(countriesMock);
 };
